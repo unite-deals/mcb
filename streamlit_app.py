ef92a53c5922fc5c4de561b6cec8c7ea0f87d4a7
unite-deals <unitedealstest1@gmail.com>
2024-02-17 10:59:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,21 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import pyperclip
 import pandas as pd
+import streamlit as st
+from PyPDF2 import PdfReader
+import pdfplumber
+import pdfplumber.display
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import pyperclip
+import pandas as pd
 
 hide_github_link_style = """
     <style>
